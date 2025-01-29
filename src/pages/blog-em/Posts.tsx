17428f65539f1de98964e230feb2a49303b54848
kimdonggu42 <kdk3280@undermilli.com>
2025-01-29 14:04:23 +0900

    feat: pagination 활성화

--- src/pages/blog-em/Posts.tsx
+++ src/pages/blog-em/Posts.tsx
@@ -6,16 +6,20 @@ import PostDetail from '@/pages/blog-em/PostDetail';
 const maxPostPage = 10;
 
 export default function Posts() {
-  const [currentPage, setCurrentPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
   const [selectedPost, setSelectedPost] = useState(null);
 
   const { data, isLoading, isError, error } = useQuery({
-    queryKey: ['posts'], // 쿼리 캐시 내의 데이터를 정의하며 항상 배열이다.
-    queryFn: () => fetchPosts(1), // 데이터를 가져오기 위해 실행할 함수
-    // 데이터를 가져온 후 2초 동안은 fresh 상태이며, 이 기간 동안 refetch가 트리거되어도 서버에서 다시 데이터를 가져오지 않는다.
+    queryKey: ['posts', currentPage], // 쿼리 캐시 내의 데이터를 정의하며 항상 배열이다.
+    queryFn: () => fetchPosts(currentPage), // 데이터를 가져오기 위해 실행할 함수
+    // 데이터를 가져온 후 5초 동안은 fresh 상태이며, 이 기간 동안 refetch가 트리거되어도 서버에서 다시 데이터를 가져오지 않는다.
     // staleTime이 지난 후에는 데이터가 stale 상태로 간주되고, refetch 트리거가 발생하면 서버에서 데이터를 다시 가져온다.
-    staleTime: 2 * 1000,
+    staleTime: 5 * 1000,
   });
+
+  const handleMovePrevPage = () => setCurrentPage((prev) => prev - 1);
+
+  const handleMoveNextPage = () => setCurrentPage((prev) => prev + 1);
 
   return (
     <>
@@ -42,11 +46,11 @@ export default function Posts() {
                 ))}
               </ul>
               <div className='flex justify-between'>
-                <button disabled onClick={() => {}}>
+                <button disabled={currentPage <= 1} onClick={handleMovePrevPage}>
                   Previous page
                 </button>
-                <span>Page {currentPage + 1}</span>
-                <button disabled onClick={() => {}}>
+                <span>Page {currentPage}</span>
+                <button disabled={currentPage >= maxPostPage} onClick={handleMoveNextPage}>
                   Next page
                 </button>
               </div>
