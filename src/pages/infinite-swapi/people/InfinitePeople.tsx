f97a3d4c6d35afd496a89c58f151983666cb8602
kimdonggu42 <kdk3280@undermilli.com>
2025-01-30 19:34:02 +0900

    feat: useInfiniteQuery 호출

--- src/pages/infinite-swapi/people/InfinitePeople.tsx
+++ src/pages/infinite-swapi/people/InfinitePeople.tsx
@@ -1,11 +1,20 @@
+import { useInfiniteQuery } from '@tanstack/react-query';
 import Person from '@/pages/infinite-swapi/people/Person';
 
 const initialUrl = 'https://swapi.py4e.com/api/people/';
-const fetchUrl = async (url: string) => {
-  const response = await fetch(url);
+const fetchUrl = async ({ pageParam }: { pageParam: string }) => {
+  const response = await fetch(pageParam);
   return response.json();
 };
 
 export default function InfinitePeople() {
+  const { data, fetchNextPage, hasNextPage } = useInfiniteQuery({
+    queryKey: ['sw-people'],
+    queryFn: fetchUrl,
+    // useInfiniteQuery를 처음 실행할 땐 pageParam이 설정되어 있지 않으므로 기본 값 initialUrl을 지정해준다.
+    initialPageParam: initialUrl,
+    getNextPageParam: (lastPage) => lastPage.next || undefined,
+  });
+
   return <></>;
 }
