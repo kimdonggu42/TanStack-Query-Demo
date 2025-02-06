9866a2ed330277e9f2e2232777563a9b9bd5b773
kimdonggu42 <kdk3280@undermilli.com>
2025-02-06 22:59:16 +0900

    feat: useCancelAppointment 훅에 mutation 적용

--- client/src/pages/lazy-days/components/appointments/hooks/useCancelAppointment.ts
+++ client/src/pages/lazy-days/components/appointments/hooks/useCancelAppointment.ts
@@ -1,20 +1,28 @@
 import { Appointment } from '../../../../../../../shared/types';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { axiosInstance } from '@/pages/lazy-days/axiosInstance/instance';
 import { useCustomToast } from '@/pages/lazy-days/components/app/hooks/useCustomToast';
 import { queryKeys } from '@/pages/lazy-days/react-query/constants';
-// for when server call is needed
-// async function removeAppointmentUser(appointment: Appointment): Promise<void> {
-//   const patchData = [{ op: 'remove', path: '/userId' }];
-//   await axiosInstance.patch(`/appointment/${appointment.id}`, {
-//     data: patchData,
-//   });
-// }
+
+const removeAppointmentUser = async (appointment: Appointment): Promise<void> => {
+  const patchData = [{ op: 'remove', path: '/userId' }];
+  await axiosInstance.patch(`/appointment/${appointment.id}`, {
+    data: patchData,
+  });
+};
 
 export const useCancelAppointment = () => {
+  const queryClient = useQueryClient();
   const toast = useCustomToast();
 
-  // TODO: replace with mutate function
-  return (appointment: Appointment) => {
-    // nothing to see here
-  };
+  const { mutate } = useMutation({
+    // Tanstack Query는 mutationFn을 실행하면서 mutate에 전달된 첫 번째 인자(appointment)를 mutationFn의 인자로 자동 전달한다.
+    mutationFn: removeAppointmentUser,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [queryKeys.appointments] });
+      toast({ title: 'You have canceled an appointment!', status: 'warning' });
+    },
+  });
+
+  return mutate;
 };
